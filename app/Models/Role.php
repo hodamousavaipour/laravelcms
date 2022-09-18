92ee3fe6a2942b4a349bd1a4bd0a7ada88325a6b
Hoda Mousavi <hoda@Hodas-MacBook-Air.local>
2022-09-18 16:15:05 +0430

    role

---
+++ app/Models/Role.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Role extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+
+    protected $fillable = ['title'];
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class,'model_permission_role','role_id','permission_id')->withPivot('model_name');
+    }
+}
