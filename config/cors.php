301c0f8ea20df169b41f9c3c1bb00197e61173d5
Hoda Mousavi <hoda@Hodas-MacBook-Air.local>
2022-09-25 16:32:53 +0330

    vocab update

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie','login','register','logout'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie','login','register','logout','user/password'],
 
     'allowed_methods' => ['*'],
 
