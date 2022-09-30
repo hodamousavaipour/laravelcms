5b8a9785cb93c3954b4ef680f904ebafa79f148b
Sepehr Amirkiee <sepehr.amirkiaee@gmail.com>
2022-09-30 22:00:05 +0330

    s

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,localhost:8080,myvocab.ir,185.143.233.120',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,localhost:8080,myvocab.ir',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
